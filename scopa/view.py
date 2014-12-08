908ca3aac3328ae1bb6c5af0e6764fdb3697d7bf
Vito De Tullio <Vito.DeTullio@gmail.com>
2014-12-08 10:32:23 +0100

    quasi

---
+++ scopa/view.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+class Application(tkinter.Frame):
+    def __init__(self):
+        tkinter.Frame.__init__(self)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.hi_there = tkinter.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+def mainloop():
+    tkinter.Tk()
+    Application().mainloop()
